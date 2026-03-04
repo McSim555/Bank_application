9c0b3fb5c449b9747d9e1d0f0aad66afe1a97355
maxim <mcsim55555k@gmail.com>
2026-03-04 17:18:21 +0300

    function sort_by_date added

--- src/processing.py
+++ src/processing.py
@@ -5,4 +5,10 @@ def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
     return new_dict_list
 
 
+def sort_by_date(operations_dates_list: list, direction: bool = True) -> list:
+    """Функцию возвращает новый список, отсртированный по дате, в начале последние операции"""
 
+    # Сортировка списка
+    new_operations_list = sorted(operations_dates_list, key=lambda x: x["date"], reverse=direction)
+
+    return new_operations_list
