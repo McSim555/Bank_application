2c0ff2a963376937975b99b5b9aa0b60aed3adfa
maxim <mcsim55555k@gmail.com>
2026-03-04 16:31:47 +0300

    errors corection

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,8 @@
-def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
+from typing import Any
+
+
+def filter_by_state(dict_list: list, state: Any = "EXECUTED") -> list:
     """Функция принимает список словарей и возвращает новый список по ключу state"""
 
-    new_dict_list = [dict for dict in dict_list if dict["state"] == state]  # Новый список словарей по ключу
+    new_dict_list = [x for x in dict_list if dict_list[state] == state]  # Новый список словарей по ключу
     return new_dict_list
