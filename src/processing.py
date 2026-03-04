030025598e81f51cf7fe12358c512bfe45d63f82
maxim <mcsim55555k@gmail.com>
2026-03-04 16:57:10 +0300

    some new errors correction

--- src/processing.py
+++ src/processing.py
@@ -3,3 +3,6 @@ def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
 
     new_dict_list = [dic_t for dic_t in dict_list if dic_t["state"] == state]  # Новый список словарей по ключу
     return new_dict_list
+
+
+
