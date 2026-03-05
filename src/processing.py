7131e42ff9386924ab458c428223e419201c7eca
maxim <mcsim55555k@gmail.com>
2026-03-05 17:11:02 +0300

    Homework 10.1 corrections

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,14 @@
-def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
+def filter_by_state(operations_list: list, state: str = "EXECUTED") -> list:
     """Функция принимает список словарей и возвращает новый список по ключу state"""
 
-    new_dict_list = [dic_t for dic_t in dict_list if dic_t["state"] == state]  # Новый список словарей по ключу
-    return new_dict_list
+    # Новый список словарей по ключу
+    new_operations_list = [operations for operations in operations_list if operations["state"] == state]
+    return new_operations_list
 
 
-def sort_by_date(operations_dates_list: list, direction: bool = True) -> list:
+def sort_by_date(operation_dates: list, direction: bool = True) -> list:
     """Функцию возвращает новый список, отсртированный по дате, в начале последние операции"""
 
     # Сортировка списка
-    new_operations_list = sorted(operations_dates_list, key=lambda x: x["date"], reverse=direction)
-
-    return new_operations_list
+    new_operations_dates = sorted(operation_dates, key=lambda x: x["date"], reverse=direction)
+    return new_operations_dates
