35cc2c929a0847a3abf4a323cf4528b189958cf5
maxim <mcsim55555k@gmail.com>
2026-03-16 11:59:39 +0300

    Homework 11.1 card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -1,122 +1,77 @@
 from typing import Generator
 
-def filter_by_currency(transactions: tuple, currency_default="USD") -> filter:
+
+def filter_by_currency(transactions: list, currency_default="USD") -> filter:
     """Функция фильтрует транзакции по валюте"""
 
-    transactions = list(transactions)
+    i = 0
     for tran in transactions:
-        if tran is not None and "code" in tran["operationAmount"]["currency"] and tran["operationAmount"]["currency"]["code"] != "":
-            continue
+        if (
+            tran is not None
+            and "code" in tran["operationAmount"]["currency"]
+            and tran["operationAmount"]["currency"]["code"] != ""
+        ):
+            if tran["operationAmount"]["currency"]["code"] == currency_default:
+                i += 1
         else:
-            raise KeyError('Отсутствует код валюты в введенных операциях или на входе пустые данные')
+            raise KeyError("Отсутствует код валюты в введенных операциях или на входе пустые данные")
 
-    transactions_filtered = (
-    filter(lambda x: x["operationAmount"]["currency"]["code"] == currency_default, transactions)
-        )
+    if i == 0:
+        raise ValueError("Нет транзакций с искомым кодом валюты")
+
+    transactions_filtered = filter(
+        lambda x: x["operationAmount"]["currency"]["code"] == currency_default, transactions
+    )
 
     return transactions_filtered
 
 
-def transaction_descriptions(transactions: tuple) -> Generator[str]:
+def transaction_descriptions(transactions: list) -> Generator[str]:
     """Генератор, который принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
 
-    transactions = list(transactions)
     if transactions is not None and len(transactions) > 0:
         for tran in transactions:
-            if tran is not None and 'description' in tran and tran['description'] != "":
+            if tran is not None and "description" in tran and tran["description"] != "":
                 continue
             else:
-                raise KeyError('Отсутствует описание в введенных операциях или на входе пустые данные')
+                raise KeyError("Отсутствует описание в введенных операциях или на входе пустые данные")
     else:
-        raise ValueError('Отсутствуют данные на входе')
-    transaction_description = (tr['description'] for tr in transactions)
+        raise ValueError("Отсутствуют данные на входе")
+    transaction_description = (tr["description"] for tr in transactions)
 
     return transaction_description
 
-# result = []
-# for _ in range(2):
-#     result = list(transaction_descriptions(([
 
-# result = []
-# for _ in range(5):
-#     result = list(transaction_descriptions((
-#     [
-#         {
-#             "id": 939719570,
-#             "state": "EXECUTED",
-#             "date": "2018-06-30T02:08:58.425572",
-#             "operationAmount": {
-#                 "amount": "9824.07",
-#                 "currency": {
-#                     "name": "USD",
-#                     "code": "USD"
-#                 }
-#             },
-#             "description": "Перевод организации",
-#             "from": "Счет 75106830613657916952",
-#             "to": "Счет 11776614605963066702"
-#         },
-#         {
-#             "id": 142264268,
-#             "state": "EXECUTED",
-#             "date": "2019-04-04T23:20:05.206878",
-#             "operationAmount": {
-#                 "amount": "79114.93",
-#                 "currency": {
-#                     "name": "USD",
-#                     "code": "USD"
-#                 }
-#             },
-#             "description": "Перевод со счета на счет",
-#             "from": "Счет 19708645243227258542",
-#             "to": "Счет 75651667383060284188"
-#         },
-#         {
-#             "id": 873106923,
-#             "state": "EXECUTED",
-#             "date": "2019-03-23T01:09:46.296404",
-#             "operationAmount": {
-#                 "amount": "43318.34",
-#                 "currency": {
-#                     "name": "руб.",
-#                     "code": "RUB"
-#                 }
-#             },
-#             "description": "Перевод со счета на счет",
-#             "from": "Счет 44812258784861134719",
-#             "to": "Счет 74489636417521191160"
-#         },
-#         {
-#             "id": 895315941,
-#             "state": "EXECUTED",
-#             "date": "2018-08-19T04:27:37.904916",
-#             "operationAmount": {
-#                 "amount": "56883.54",
-#                 "currency": {
-#                     "name": "USD",
-#                     "code": "USD"
-#                 }
-#             },
-#             "description": "Перевод с карты на карту",
-#             "from": "Visa Classic 6831982476737658",
-#             "to": "Visa Platinum 8990922113665229"
-#         },
-#         {
-#             "id": 594226727,
-#             "state": "CANCELED",
-#             "date": "2018-09-12T21:27:25.241689",
-#             "operationAmount": {
-#                 "amount": "67314.70",
-#                 "currency": {
-#                     "name": "руб.",
-#                     "code": "RUB"
-#                 }
-#             },
-#             "description": "Перевод организации",
-#             "from": "Visa Platinum 1246377376343588",
-#             "to": "Счет 14211924144426031657"
-#         }
-#     ]
-# )))
-#
-# print(result)
+def card_number_generator(start_card_number: str, end_card_number: str) -> Generator[str]:
+    """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
+
+    if start_card_number == "" or end_card_number == "":
+        raise ValueError('Не указаны начало и/или конец диапазона')
+
+    if len(start_card_number) > 16 or len(end_card_number) >16:
+        raise ValueError('Максимальное количество цифр в номере карты 16')
+
+    if start_card_number.isdigit() and end_card_number.isdigit():
+        start_card_number = int(start_card_number)
+        end_card_number = int(end_card_number)
+    else:
+        raise TypeError('Неверный формат цифр диапазона')
+
+    if start_card_number > end_card_number:
+        raise ValueError("Начальный номер карты больше конечного номера")
+    else:
+        i = start_card_number
+        while i <= end_card_number:
+            card_number = i
+            card_number_str = str(card_number).zfill(16)
+            card_number_final = (
+                card_number_str[:4]
+                + " "
+                + card_number_str[4:8]
+                + " "
+                + card_number_str[8:12]
+                + " "
+                + card_number_str[12:]
+            )
+            yield card_number_final
+            i += 1
