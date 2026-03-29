9b6efd651b08b8b75e588960aa2c72a395fe14f8
maxim <mcsim55555k@gmail.com>
2026-03-29 17:02:33 +0300

    Homework 12.1 second function

---
+++ src/external_api.py
@@ -0,0 +1,46 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+
+def conversion_to_rub(transaction: dict) -> float:
+    """функцию принимает на вход транзакцию и возвращает сумму транзакции в рублях, если транзакция была в USD или EUR,
+    происходит обращение к внешнему API для получения текущего курса валют и конвертации суммы операции в рубли"""
+
+    if transaction["operationAmount"]["currency"]["code"] == "RUB":
+        amount = transaction["operationAmount"]["amount"]
+        return amount
+
+    elif (
+        transaction["operationAmount"]["currency"]["code"] == "EUR"
+        or transaction["operationAmount"]["currency"]["code"] == "USD"
+    ):
+        url = "https://api.apilayer.com/exchangerates_data/convert"
+        load_dotenv("../.env")
+        API_KEY = os.getenv("API_KEY")
+        headers = {"apikey": API_KEY}
+        if transaction["operationAmount"]["currency"]["code"] == "EUR":
+            payload = {"amount": transaction["operationAmount"]["amount"], "from": "EUR", "to": "RUB"}
+        else:
+            payload = {"amount": transaction["operationAmount"]["amount"], "from": "USD", "to": "RUB"}
+
+        response = requests.get(url, headers=headers, params=payload)
+
+        result = response.json()["result"]
+        return result
+
+    else:
+        raise ValueError("Конвертация такой валюты не предусмотрена")
+
+
+# transaction = {
+#     "id": 716496732,
+#     "state": "EXECUTED",
+#     "date": "2018-04-04T17:33:34.701093",
+#     "operationAmount": {"amount": "40701.91", "currency": {"name": "EUR", "code": "EUR"}},
+#     "description": "Перевод организации",
+#     "from": "Visa Gold 5999414228426353",
+#     "to": "Счет 72731966109147704472",
+# }
+# r = conversion_to_rub(transaction)
+# print(r)
--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ def financial_transactions(path_source: str) -> list[dict]:
     """Функцию принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях"""
 
     try:
-        with open(path_source, 'r', encoding='utf-8') as file:
+        with open(path_source, "r", encoding="utf-8") as file:
             transactions = json.load(file)
             if type(transactions) == list and len(transactions) > 0:
                 return transactions
@@ -15,7 +15,6 @@ def financial_transactions(path_source: str) -> list[dict]:
     except Exception:
         return []
 
+
 # p = r"C:\Users\79161\Documents\Skypro\Python_projects\bank_application\data\operations.json"
 # print(financial_transactions(p))
-
-
